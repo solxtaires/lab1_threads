da8db7f64321ce2d2386984b5856a5f287c72376
Jason Sin <jason.sin562@gmail.com>
2026-02-26 22:31:02 -0800

    Finished coding section of lab1

--- lab1_threads.c
+++ lab1_threads.c
@@ -8,24 +8,53 @@ CECS 326 Lab 1 - Threads in C
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <unisted.h>
+#include <unistd.h>
 
 void* worker_thread(void* arg) {
 	(void)arg;
 
 	while (1) {
 		sleep(3); //sleep is a cancellation point on POSIX systems
-		printf("[worker] I'm alive! (printed every 3 seconds\n");
+		printf("Worker thread is running...\n");
 		fflush(stdout);
 	}
 
 	return NULL;
 }
 
-int main(void) {
-	pthread_t tid;
-
-	if (pthread_create(&tid, NULL, worker_thread, NULL) != 0) {
-		perror("pthread_create");
+int main() {
+	pthread_t thread_id;
+	// created a new thread
+	if (pthread_create(&thread_id, NULL, worker_thread, NULL) != 0) {
+		perror("Error creating thread");
 		return 1;
 	}
+	// Give user instructions on what to do
+	printf("Press Enter to cancel the worker thread...\n");
+	getchar(); //wait for Enter key
+	printf("Canceling worker thread...\n");
+
+	if (pthread_cancel(thread_id) !=0) {
+		perror("Error canceling thread");
+		return 1;
+	}
+
+	/* send cancellation request to worker thread
+	thread will stop at next cancellation point
+	*/
+
+	if (pthread_join(thread_id, NULL) != 0) {
+		perror("pthread_join");
+		return 1;
+	}
+
+	printf("Worker thread canceled successfully.\n");
+	printf("Waiting 5 seconds before exiting...\n");
+
+	// wait 5 seconds to show that worker is gone
+	sleep(5);
+
+	printf("Main thread eixting.\n");
+
+	return 0;
+}
